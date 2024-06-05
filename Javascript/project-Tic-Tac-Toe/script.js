22cdf38e61c0297726ce3b0f14a3bb86bd3f6a30
kawrou <121672581+kawrou@users.noreply.github.com>
2024-06-05 14:47:28 +0100

    Finished general flow of game. Bug with resetting game.

--- Javascript/project-Tic-Tac-Toe/script.js
+++ Javascript/project-Tic-Tac-Toe/script.js
@@ -1,36 +1,61 @@
-const gameBoard = (function () {
+const GameBoard = (function () {
   let board = [
     [0, 0, 0],
     [0, 0, 0],
     [0, 0, 0],
   ];
 
+  let originalBoard = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+  ];
+
+  const print = () => {
+    console.log(board.map((row) => row.join("|")).join("\n"));
+  };
+
+  const markCell = (playerId, row, col) => {
+    if ((board[row][col] === 0)) {
+      board[row][col] = playerId;
+      print();
+    } else {
+      console.log("Cell already marked. Choose another cell");
+    }
+  };
+
+  const resetBoard = () => {
+    board = originalBoard.map(row => row.slice()); 
+		console.log("Cleared board")
+		print(); 
+  };
+
   return {
     board,
-    print: () =>
-      console.log(this.board.map((row) => row.join(" | ")).join("\n")),
+    print,
+    markCell,
+    resetBoard,
   };
 })();
 
-function ticTacToe(player1, player2) {
+function TicTacToe(player1, player2) {
   let isPlayer1Turn = true;
+  let currentPlayer;
+
+  const checkCurrentPlayer = () => {
+    return (currentPlayer = isPlayer1Turn ? player1 : player2);
+  };
 
   function markCell(currentPlayer, rowSelection, collumnSelection) {
-    let currentPlayer = isPlayer1Turn ? player1 : player2;
-    let row = rowSelection;
-    let col = collumnSelection;
-
-    if (gameBoard[row][col] === 0) {
-      gameBoard[row][col] = currentPlayer.name;
-      isPlayer1Turn = !isPlayer1Turn;
-      gameBoard.board.print();
-      checkWin(currentPlayer);
-    } else {
-      console.log("Cell already marked. Choose another cell");
-      markCell(currentPlayer, row, col);
-    }
+    GameBoard.print();
+    GameBoard.markCell(currentPlayer, rowSelection, collumnSelection);
+    GameBoard.print();
+    isPlayer1Turn = !isPlayer1Turn;
   }
 
+  const resetGameState = () => {
+    GameBoard.resetBoard();
+  };
   // prettier-ignore
   const winCombinations = [
 	// Rows
@@ -49,26 +74,76 @@ function ticTacToe(player1, player2) {
 	];
 
   function checkWin(player) {
+		console.log(player)
     for (const combination of winCombinations) {
       if (
-        combination.every(
-          ([row, col]) => gameBoard.board[row][col] === player.name
-        )
+        combination.every(([row, col]) => GameBoard.board[row][col] === player)
       ) {
         return true;
       }
     }
     return false;
   }
+  return {
+    checkCurrentPlayer,
+    markCell,
+    checkWin,
+    resetGameState,
+  };
 }
 
-function createPlayer(name) {
-  return { name };
+function CreatePlayer(name, id) {
+  return { name, id };
 }
 
-const player1 = createPlayer("x");
-const player2 = createPlayer("y");
+function GameController() {
+  let player1;
+  let player2;
+  let game;
 
-ticTacToe(player1, player2);
+  const createPlayers = () => {
+    const player1Name = prompt("Enter name for player 1");
+    const player2name = prompt("Enter name for player 2");
+    player1 = CreatePlayer(player1Name, 1);
+    player2 = CreatePlayer(player2name, 2);
+  };
 
-console.prompt()
+  const resetGame = () => {
+    game.resetGameState();
+		console.log("Game Reset")
+		askMove();
+  };
+
+  function startGame() {
+    createPlayers();
+    game = TicTacToe(player1, player2);
+    askMove();
+  }
+
+  const askMove = () => {
+    const currentPlayer = game.checkCurrentPlayer();
+    const rowSelection = prompt(`${currentPlayer.name} enter the row:`);
+    const collumnSelection = prompt("Enter the column");
+    game.markCell(
+      currentPlayer.id,
+      Number(rowSelection),
+      Number(collumnSelection)
+    );
+    if (game.checkWin(currentPlayer.id)) {
+      console.log(`${currentPlayer.name} win!`);
+      const playAgain = prompt("Play again? (yes/no)").toLowerCase() === "yes";
+      if (playAgain) {
+        resetGame();
+      }
+    } else {
+      askMove();
+    }
+  };
+
+  return {
+    startGame,
+  };
+}
+
+const game = GameController();
+
