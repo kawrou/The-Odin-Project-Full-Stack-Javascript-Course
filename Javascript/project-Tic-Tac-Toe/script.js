682647aa05eb6fe9bcb29492ba54fb3d7a118428
kawrou <121672581+kawrou@users.noreply.github.com>
2024-06-07 17:49:04 +0100

    Browser UI is interactive and renders game state

--- Javascript/project-Tic-Tac-Toe/script.js
+++ Javascript/project-Tic-Tac-Toe/script.js
@@ -1,6 +1,6 @@
 // CreatePlayer Factory Function
-function CreatePlayer(name, id) {
-  return { name, id };
+function CreatePlayer(name, symbol) {
+  return { name, symbol };
 }
 
 // GameBoard class
@@ -53,7 +53,7 @@ const GameBoard = (() => {
 })();
 
 //TicTacToe Logic
-function TicTacToe(player1, player2, GameBoard) {
+function TicTacToe(player1, player2, gameBoard) {
   let isPlayer1Turn = true;
   let currentPlayer;
 
@@ -61,13 +61,9 @@ function TicTacToe(player1, player2, GameBoard) {
     return (currentPlayer = isPlayer1Turn ? player1 : player2);
   };
 
-  function markCell(rowSelection, collumnSelection) {
+  function markCell(row, col) {
     const currentPlayer = checkCurrentPlayer();
-    const success = GameBoard.markCell(
-      currentPlayer.id,
-      rowSelection,
-      collumnSelection
-    );
+    const success = gameBoard.markCell(currentPlayer.symbol, row, col);
     if (!success) {
       return false;
     }
@@ -79,7 +75,7 @@ function TicTacToe(player1, player2, GameBoard) {
   };
 
   const resetGameState = () => {
-    GameBoard.clearBoard();
+    gameBoard.clearBoard();
   };
   // prettier-ignore
   const winCombinations = [
@@ -87,19 +83,20 @@ function TicTacToe(player1, player2, GameBoard) {
     [[0, 0], [0, 1], [0, 2]],
       [[1, 0], [1, 1], [1, 2]],
       [[2, 0], [2, 1], [2, 2]],
-  
+
       // Columns
       [[0, 0], [1, 0], [2, 0]],
       [[0, 1], [1, 1], [2, 1]],
       [[0, 2], [1, 2], [2, 2]],
-  
+
       // Diagonals
       [[0, 0], [1, 1], [2, 2]],
       [[0, 2], [1, 1], [2, 0]]
       ];
 
   function checkIsWon() {
-    const isDraw = GameBoard.getGameBoard()
+    const isDraw = gameBoard
+      .getGameBoard()
       .flat()
       .every((cell) => cell === "x" || cell === "o");
     if (isDraw) {
@@ -110,7 +107,7 @@ function TicTacToe(player1, player2, GameBoard) {
       if (
         combination.every(
           ([row, col]) =>
-            GameBoard.getGameBoard()[row][col] === currentPlayer.id
+            gameBoard.getGameBoard()[row][col] === currentPlayer.symbol
         )
       ) {
         return { winner: currentPlayer, isDraw: false };
@@ -129,85 +126,168 @@ function TicTacToe(player1, player2, GameBoard) {
 }
 
 //Control the flow of the game
-function GameController() {
-  let player1;
-  let player2;
-  let game;
+function GameController(createPlayer, gameBoard) {
+  // let player1;
+  // let player2;
+  // let game;
 
-  const createPlayers = () => {
-    const player1Name = prompt("Enter name for player 1");
-    const player2name = prompt("Enter name for player 2");
-    player1 = CreatePlayer(player1Name, "x");
-    player2 = CreatePlayer(player2name, "y");
-  };
+  // const createPlayers = () => {
+  //   const player1Name = prompt("Enter name for player 1");
+  //   const player2name = prompt("Enter name for player 2");
+  //   player1 = createPlayer(player1Name, "x");
+  //   player2 = createPlayer(player2name, "y");
+  // };
+
+  const player1 = createPlayer("player1", "x");
+  const player2 = createPlayer("player2", "o");
+  let game;
 
   const resetGame = () => {
     game.resetGameState();
-    console.log("Game Reset");
-    askMove();
   };
 
   function startGame() {
-    createPlayers();
-    game = TicTacToe(player1, player2, GameBoard);
-    askMove();
+    // createPlayers();
+    game = TicTacToe(player1, player2, gameBoard);
   }
 
   function logGameBoard() {
     console.log(
-      GameBoard.getGameBoard()
+      gameBoard
+        .getGameBoard()
         .map((row) => row.join("|"))
         .join("\n")
     );
   }
-  const askMove = () => {
-    logGameBoard();
-    const currentPlayer = game.checkCurrentPlayer();
-    const rowSelection = prompt(`${currentPlayer.name} enter the row:`);
-    const collumnSelection = prompt("Enter the column");
 
-    const success = game.markCell(
-      Number(rowSelection),
-      Number(collumnSelection)
-    );
+  const makeMove = (row, col) => {
+    const success = game.markCell(row, col);
 
     if (!success) {
-      askMove();
+      return false;
     }
-
-    //Runs only if no one has won and more moves can be made.
-    if (checkWinState() === false) {
-      game.switchPlayerTurn();
-      askMove();
-    }
+    game.switchPlayerTurn();
+    return gameBoard.getGameBoard();
   };
 
   const checkWinState = () => {
     const winState = game.checkIsWon();
+
     if (winState.isDraw) {
-      logGameBoard();
       console.log("The game is a draw!");
+      return { isDraw: true, winner: null };
     }
 
     if (winState.winner === null && !winState.isDraw) {
       console.log("Next players turn");
-      return false;
+      return null;
     }
 
     if (!winState.isDraw) {
-      logGameBoard();
       console.log(`${winState.winner.name} is the winner!`);
-    }
-
-    const playAgain = prompt("Play again? (yes/no)").toLowerCase() === "yes";
-    if (playAgain) {
-      resetGame();
+      return { isDraw: false, winner: winState.winner };
     }
   };
 
   return {
     startGame,
+    resetGame,
+    makeMove,
+    checkWinState,
   };
 }
 
-const game = GameController();
+function UIController(gameController) {
+  const boardContainer = document.getElementById("board-container");
+  const winnerMessage = document.getElementById("winner-message");
+  const playAgainButton = document.getElementById("play-again-btn");
+
+  const generateBoardHtml = () => {
+    while (boardContainer.firstChild) {
+      boardContainer.removeChild(boardContainer.firstChild);
+    }
+
+    for (let row = 0; row < 3; row++) {
+      for (let col = 0; col < 3; col++) {
+        const cell = document.createElement("div");
+        cell.classList.add("cell");
+        cell.setAttribute("data-row", row);
+        cell.setAttribute("data-col", col);
+
+        cell.addEventListener("click", () => {
+          const success = gameController.makeMove(row, col);
+          if (success) {
+            refreshBoard(success);
+            const winState = gameController.checkWinState();
+
+            if (!winState) {
+              return;
+            }
+
+            if (winState.winner || winState.isDraw) {
+              handleGameEnd(winState);
+            }
+          }
+        });
+        boardContainer.appendChild(cell);
+      }
+    }
+  };
+
+  playAgainButton.addEventListener("click", () => {
+    playAgain();
+    hidePlayAgainButton();
+  });
+
+  function hidePlayAgainButton() {
+    playAgainButton.style.display = "none";
+  }
+
+  function showPlayAgainButton() {
+    playAgainButton.style.display = "block";
+  }
+
+  const refreshBoard = (gameBoard) => {
+    const cells = document.querySelectorAll(".cell");
+    cells.forEach((cell) => {
+      const row = cell.getAttribute("data-row");
+      const col = cell.getAttribute("data-col");
+
+      if (gameBoard[row][col] === 0) {
+        console.log(gameBoard[row][col]);
+        cell.textContent = "";
+      } else {
+        cell.textContent = gameBoard[row][col];
+      }
+    });
+  };
+
+  const handleGameEnd = (winState) => {
+    if (winState.isDraw) {
+      winnerMessage.textContent = "The game is a drawer!";
+    } else {
+      winnerMessage.textContent = `${winState.winner.name} is the winner!`;
+    }
+    showPlayAgainButton();
+  };
+
+  function playAgain() {
+    gameController.resetGame();
+    generateBoardHtml();
+  }
+
+  return {
+    generateBoardHtml,
+  };
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const gameController = GameController(CreatePlayer, GameBoard);
+  const uiController = UIController(gameController);
+  uiController.generateBoardHtml();
+  const startGameButton = document.getElementById("start-game-btn");
+  startGameButton.addEventListener("click", () => {
+    gameController.startGame();
+    startGameButton.style.display = "none";
+  });
+});
